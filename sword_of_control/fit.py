fab9ed10170b6f5981723a4187cc4b39ce6e3401
nullJaX <195643@edu.p.lodz.pl>
2019-06-19 17:09:27 +0200

    Sol

--- sword_of_control/fit.py
+++ sword_of_control/fit.py
@@ -1,1 +1,108 @@
 #!/usr/bin/env python3
+import itertools
+import json
+import random
+from threading import Thread
+
+from tqdm import tqdm
+
+from sword import morphology_map, where_da_sword
+
+
+def iou(expected, output):
+    xA = max(expected[0], output[0])
+    yA = max(expected[1], output[1])
+    xB = min(expected[2], output[2])
+    yB = min(expected[3], output[3])
+
+    # compute the area of intersection rectangle
+    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    expectedArea = (expected[2] - expected[0] + 1) * (expected[3] - expected[1] + 1)
+    outputArea = (output[2] - output[0] + 1) * (output[3] - output[1] + 1)
+
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = interArea / (float(expectedArea + outputArea - interArea) + 1.0)
+
+    # return the intersection over union value
+    return iou
+
+
+def load_data(i):
+    data = []
+    with open("./train/out{0!s}.txt".format(i)) as f:
+        for line in f:
+            line = line.replace("\n", "")
+            line = line.replace("#", "-1")
+            line = line.split(";")
+            line = tuple(map(int, line))
+            data.append(line)
+    return data
+
+
+def validate(j, params, iou_for_params):
+    expected = load_data(j)
+    iou_for_file = []
+    for i, frame in enumerate(where_da_sword("./train/in{0!s}.mp4".format(j), expected[0], best_params)):
+        iou_for_file.append(iou(expected[i], frame))
+    iou_for_params.append(sum(iou_for_file) / len(iou_for_file))
+
+
+if __name__ == '__main__':
+    with open("config.json", "r") as f:
+        best_params = dict(json.load(f))
+    best_iou = []
+    for j in tqdm(range(1, 9, 1)):
+        expected = load_data(j)
+        iou_for_file = []
+        for i, frame in enumerate(where_da_sword("./train/in{0!s}.mp4".format(j), expected[0], best_params)):
+            iou_for_file.append(iou(expected[i], frame))
+        best_iou.append(sum(iou_for_file) / len(iou_for_file))
+    best_iou = sum(best_iou) / len(best_iou)
+    print("Best IoU: {0!s}".format(best_iou))
+
+    deque_maxlen = [i for i in range(1, 500, 2)]
+    blur_kernel_size = [i for i in range(1, 15, 2)]
+    canny_threshold1 = [i for i in range(0, 300, 1)]
+    canny_threshold2 = [i for i in range(0, 300, 1)]
+    hough_rho = [i/10 for i in range(0, 100, 1)]
+    hough_threshold = [i for i in range(0, 500, 2)]
+    hough_minLineLength = [i for i in range(0, 200, 1)]
+    hough_maxLineGap = [i for i in range(0, 500, 1)]
+    morphology_method = [i for i in morphology_map.keys()]
+    morphology_kernel = [i for i in range(1, 21, 2)]
+    morphology_iter = [i for i in range(0, 10, 1)]
+
+    for _ in tqdm(range(50000)):
+        params = {
+            "deque_maxlen": random.choice(deque_maxlen),
+            "blur_kernel_size": random.choice(blur_kernel_size),
+            "canny_threshold1": random.choice(canny_threshold1),
+            "canny_threshold2": random.choice(canny_threshold2),
+            "hough_rho": random.choice(hough_rho),
+            "hough_threshold": random.choice(hough_threshold),
+            "hough_minLineLength": random.choice(hough_minLineLength),
+            "hough_maxLineGap": random.choice(hough_maxLineGap),
+            "morphology_method": random.choice(morphology_method),
+            "morphology_kernel": random.choice(morphology_kernel),
+            "morphology_iter": random.choice(morphology_iter)
+        }
+        threads = []
+        iou_for_params = []
+        for j in range(1, 9, 1):
+            threads.append(Thread(target=validate, args=(j, params, iou_for_params)))
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
+        iou_for_params = sum(iou_for_params) / len(iou_for_params)
+        if iou_for_params > best_iou:
+            best_params = params
+            best_iou = iou_for_params
+            print("Better IoU: {0!s}".format(best_iou))
+    with open("config.json", "w") as f:
+        json.dump(best_params, f)
--- sword_of_control/sword.py
+++ sword_of_control/sword.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 import sys
 import json
+from collections import deque
+
 import cv2
 import numpy as np
+
+morphology_map = {"dilate": cv2.MORPH_DILATE,
+                  "close": cv2.MORPH_CLOSE,
+                  "blackhat": cv2.MORPH_BLACKHAT,
+                  "cross": cv2.MORPH_CROSS,
+                  "ellipse": cv2.MORPH_ELLIPSE,
+                  "erode": cv2.MORPH_ERODE,
+                  "gradient": cv2.MORPH_GRADIENT,
+                  "hitmiss": cv2.MORPH_HITMISS,
+                  "open": cv2.MORPH_OPEN,
+                  "rect": cv2.MORPH_RECT,
+                  "tophat": cv2.MORPH_TOPHAT}
 
 
 def distance(last, current):
@@ -16,15 +30,18 @@ def distance(last, current):
     return (dist_a + dist_b + dist_c) / 3
 
 
-def compare(last_frame, frame, last_position, params):
+def compare(last_frame, frame, last_positions, params):
     # difference between frames
     frame_diff = cv2.absdiff(frame, last_frame)
+    frame_diff = cv2.morphologyEx(frame_diff, morphology_map[params["morphology_method"]], (params["morphology_kernel"], params["morphology_kernel"]), iterations=params["morphology_iter"])
 
     # Limit Region Of Interest by creating circle of radius equal to longer dimension delta
-    # radius = max(last_position[3]-last_position[1], last_position[2]-last_position[0])
-    # mask = np.zeros(frame_diff.shape, dtype=np.uint8)
-    # cv2.circle(mask, last_position[-2:], radius, (255, 255, 255), -1, 8, 0)
-    # frame_diff = frame_diff & mask
+    mask = np.zeros(frame_diff.shape, dtype=np.uint8)
+    for roi in last_positions:
+        radius = max(abs(roi[3] - roi[1]), abs(roi[2] - roi[0]))
+        cv2.circle(mask, roi[-2:], radius, (255, 255, 255), -1, 8, 0)
+    frame_diff = frame_diff & mask
+    # cv2.imshow("CannyEdges", frame_diff)
 
     # Detect edges
     canny = cv2.Canny(frame_diff, params["canny_threshold1"], params["canny_threshold2"])
@@ -32,17 +49,19 @@ def compare(last_frame, frame, last_position, params):
                             params["hough_threshold"],
                             minLineLength=params["hough_minLineLength"],
                             maxLineGap=params["hough_maxLineGap"])
+    # frame_diff = cv2.cvtColor(frame_diff, cv2.COLOR_GRAY2BGR)
     # cv2.imshow("CannyEdges", canny)
+
 
     # Determine new points
     if lines is not None:
-        distances = [distance(last_position, line[0]) for line in lines]
+        distances = [distance(last_positions[-1], line[0]) for line in lines]
         index = distances.index(min(distances))
         xa, ya, xb, yb = lines[index][0]
         xc = int(np.round((xa + xb) / 2))
         yc = int(np.round((ya + yb) / 2))
     else:
-        xa, ya, xb, yb, xc, yc = last_position
+        xa, ya, xb, yb, xc, yc = last_positions[-1]
     return xa, ya, xb, yb, xc, yc
 
 
@@ -67,28 +86,29 @@ def mp4_read(video_file_path):
 def where_da_sword(video_file_path, first_frame_position, params):
     x_c = int(np.round(sum(first_frame_position[0::2]) / 2))
     y_c = int(np.round(sum(first_frame_position[1::2]) / 2))
-    last_position = (*first_frame_position, x_c, y_c)
+    last_positions = deque([(*first_frame_position, x_c, y_c)], maxlen=params["deque_maxlen"])
     last_frame = None
     for i, frame in enumerate(mp4_read(video_file_path)):
         if i == 0:
             last_frame = preprocess(frame, params)
         else:
             frame = preprocess(frame, params)
-            last_position = tuple(map(int, compare(last_frame, frame, last_position, params)))
+            last_positions.append(tuple(map(int, compare(last_frame, frame, last_positions, params))))
             last_frame = frame.copy()
-        x_a, y_a, x_b, y_b, _, _ = last_position
+        x_a, y_a, x_b, y_b, x_c, y_c = last_positions[-1]
         height, width = last_frame.shape[:2]
         x_a = x_a if 0 <= x_a < width else -1
         x_b = x_b if 0 <= x_b < width else -1
         y_a = y_a if 0 <= y_a < height else -1
         y_b = y_b if 0 <= y_b < height else -1
-        display = cv2.cvtColor(last_frame, cv2.COLOR_GRAY2RGB)
-        if all([i >= 0 for i in [x_a, x_b, y_a, y_b]]):
-            cv2.line(display, (x_a, y_a), (x_b, y_b), (0, 255, 0), 3)
-        cv2.circle(display, (x_a, y_a), 5, (255, 0, 0), -1)
-        cv2.circle(display, (x_b, y_b), 5, (0, 0, 255), -1)
-        cv2.imshow('Debug display', display)
-        cv2.waitKey(30)
+        # display = cv2.cvtColor(last_frame, cv2.COLOR_GRAY2RGB)
+        # if all([i >= 0 for i in [x_a, x_b, y_a, y_b]]):
+        #     cv2.line(display, (x_a, y_a), (x_b, y_b), (0, 255, 0), 10)
+        # cv2.circle(display, (x_a, y_a), 5, (255, 0, 0), -1)
+        # cv2.circle(display, (x_b, y_b), 5, (0, 0, 255), -1)
+        # cv2.circle(display, (x_c, y_c), 5, (0, 0, 0), -1)
+        # cv2.imshow('Debug display', display)
+        # cv2.waitKey(30)
         yield x_a, y_a, x_b, y_b
 
 
